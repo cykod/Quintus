61d9c5c8547659b310afcc4eac5bdd444f1f9e04
Lee Rowlands <lee.rowlands@previousnext.com.au>
2014-01-04 18:55:00 +1000

    Use touch controls for up/down

--- examples/platformer_full/platformer.js
+++ examples/platformer_full/platformer.js
@@ -17,7 +17,7 @@ var Q = window.Q = Quintus()
         // Maximize this game to whatever the size of the browser is
         .setup({ maximize: true })
         // And turn on default input controls and touch input (for UI)
-        .controls().touch()
+        .controls(true).touch()
 
 Q.Sprite.extend("Player",{
 
